1803cbd34bec97ca047442b5c55dc93506a4f6dd
kinhosz <jcsc@cin.ufpe.br>
2022-03-22 01:52:42 -0300

    refactor(query): refactor sql queries

--- app/db/query.py
+++ app/db/query.py
@@ -1,11 +1,6 @@
 from app.db import handshake
 
-def find_equal(table, column, equal_to, view = ['*']):
-    sql_query = 'SELECT ' + ', '.join(view)
-    sql_query = sql_query + ' FROM ' + table
-    sql_query = sql_query + ' WHERE ' + column + ' = \'' + equal_to + '\''
-
-    print("Query: ", sql_query)
+def execute(sql_query):
     conn = handshake.connect()
     cursor = conn.cursor()
     
@@ -18,4 +13,18 @@ def find_equal(table, column, equal_to, view = ['*']):
     handshake.disconnect(conn)
 
     return response
+
+def find_equal(table, column, equal_to, view = ['*']):
+    sql_query = 'SELECT ' + ', '.join(view)
+    sql_query = sql_query + ' FROM ' + table
+    sql_query = sql_query + ' WHERE ' + column + ' = \'' + equal_to + '\''
+
+    return execute(sql_query)
+
+def less_than(table, column, less_than, view = ['*']):
+    sql_query = 'SELECT ' + ', '.join(view)
+    sql_query = sql_query + ' FROM ' + table
+    sql_query = sql_query + ' WHERE ' + column + ' = ' + less_than
+
+    return execute(sql_query)
 
