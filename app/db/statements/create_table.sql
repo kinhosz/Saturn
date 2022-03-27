f8e2b8d4f24fd29e80b740a1c7d6dde8b13e67e6
kinhosz <jcsc@cin.ufpe.br>
2022-03-27 02:55:47 -0300

    fix(db/create_table): replace serial to int

--- app/db/statements/create_table.sql
+++ app/db/statements/create_table.sql
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS orders(
 
 CREATE TABLE IF NOT EXISTS trades(
     id SERIAL PRIMARY KEY,
-    user_id SERIAL NOT NULL REFERENCES users(id),
-    order_bought_id SERIAL REFERENCES orders(id),
-    order_sold_id SERIAL REFERENCES orders(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    order_bought_id INTEGER REFERENCES orders(id),
+    order_sold_id INTEGER REFERENCES orders(id),
     quantity_sold REAL,
     quantity_bought REAL
 );
