7883430a67b461ebb2d70a0446b85b57a5a0492b
kinhosz <jcsc@cin.ufpe.br>
2022-04-22 00:11:31 -0300

    feat(models/trade): add trade model

--- app/models/trade.py
+++ app/models/trade.py
@@ -9,3 +9,62 @@ class Trade(object):
         self.__open = None
         self.__walletID = None
 
+    def purchasePriceBRL(self):
+        if self.__purchasePriceBRL != None:
+            return self.__purchasePriceBRL
+        
+        sql = "SELECT purchase_price_BRL " \
+            + "FROM trades " \
+            + "WHERE id = " + str(self.__id)
+        
+        self.__purchasePriceBRL = db.manual(sql)[0][0]
+
+        return self.__purchasePriceBRL
+    
+    def salePriceBRL(self):
+        if self.__salePriceBRL != None:
+            return self.__salePriceBRL
+        
+        sql = "SELECT sale_price_BRL " \
+            + "FROM trades " \
+            + "WHERE id = " + str(self.__id)
+        
+        self.__salePriceBRL = db.manual(sql)[0][0]
+
+        return self.__salePriceBRL
+    
+    def quantityBTC(self):
+        if self.__quantityBTC != None:
+            return self.__quantityBTC
+        
+        sql = "SELECT quantity_BTC " \
+            + "FROM trades " \
+            + "WHERE id = " + str(self.__id)
+        
+        self.__quantityBTC = db.manual(sql)[0][0]
+
+        return self.__quantityBTC
+    
+    def open(self):
+        if self.__open != None:
+            return self.__open
+        
+        sql = "SELECT open " \
+            + "FROM trades " \
+            + "WHERE id = " + str(self.__id)
+        
+        self.__open = db.manual(sql)[0][0]
+
+        return self.__open
+    
+    def walletID(self):
+        if self.__walletID != None:
+            return self.__walletID
+        
+        sql = "SELECT wallet_id " \
+            + "FROM trades " \
+            + "WHERE id = " + str(self.__id)
+        
+        self.__walletID = db.manual(sql)[0][0]
+
+        return self.__walletID
