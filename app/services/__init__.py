cb2dd9d186fb2e6211c3c3c359a257b122ec4451
kinhosz <jcsc@odoo.com>
2025-07-23 00:05:09 +0200

    [REF] services: rem find_sellable_balances

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,6 +3,5 @@ from .activate_users import activate_users
 from .create_balances import create_balances
 from .create_trading_settings import create_trading_settings
 from .find_buyable_balances import find_buyable_balances
-from .find_sellable_balances import find_sellable_balances
 
 from .utils import *
