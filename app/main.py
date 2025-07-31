1f78b65100e209f1d110035469f1f48d514f4434
kinhosz <jcsc@odoo.com>
2025-07-31 22:15:41 +0200

    [FEAT] main: logger

--- app/main.py
+++ app/main.py
@@ -3,11 +3,24 @@ from dotenv import load_dotenv
 from app import render, db_config, set_env
 
 import logging
+from colorlog import ColoredFormatter
 
-logging.basicConfig(
-  level=logging.INFO,
-  format="%(asctime)s [%(levelname)s] %(message)s"
+handler = logging.StreamHandler()
+formatter = ColoredFormatter(
+    "%(log_color)s%(asctime)s [%(levelname)s] %(name)s - %(message)s",
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    }
 )
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 def main():
   load_dotenv()
