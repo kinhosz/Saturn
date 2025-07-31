2db61139e69e58369ef5d34886a2f85cf4b9c8b1
kinhosz <jcsc@odoo.com>
2025-07-31 23:02:09 +0200

    [FIX] main: rmv colorama of logger

--- app/main.py
+++ app/main.py
@@ -3,24 +3,11 @@ from dotenv import load_dotenv
 from app import render, db_config, set_env
 
 import logging
-from colorlog import ColoredFormatter
 
-handler = logging.StreamHandler()
-formatter = ColoredFormatter(
-    "%(log_color)s%(asctime)s [%(levelname)s] %(name)s - %(message)s",
-    log_colors={
-        "DEBUG": "cyan",
-        "INFO": "green",
-        "WARNING": "yellow",
-        "ERROR": "red",
-        "CRITICAL": "bold_red",
-    }
+logging.basicConfig(
+  level=logging.INFO,
+  format="%(asctime)s [%(levelname)s] %(name)s - %(message)s"
 )
-handler.setFormatter(formatter)
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
 
 def main():
   load_dotenv()
